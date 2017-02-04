8246125e9138db652fec7b748c9937fd50500852
aplegrand <alegrand1104@gmail.com>
2017-02-04 13:20:08 -0600

    Implemented fortune cookie code

--- main.py
+++ main.py
@@ -15,10 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    #list of fortunes
+    fortunes = [
+    "Man who run in front of car get tired. Man who run behind car get exhausted.",
+    "Man who stand on toilet is high on pot.",
+    "Man who want pretty nurse must be patient.",
+    "Woman who put man in doghouse will find him in cathouse."
+    ]
+    #randomly select a fortune
+    i = random.randrange(0, len(fortunes))
+    return fortunes[i]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() +"</strong>"
+        fort_sentence = "Your fortune: "
+        fort_paragraph = "<p>" + fort_sentence + fortune + "</p>"
+
+        lucky_number = random.randint(1,100)
+        num_print = 'Your lucky number is: <strong>' + str(lucky_number) + "</strong>"
+        num_paragraph = "<p>" + num_print + "</p>"
+
+        another = '''<a href="."><button>Another cookie please!</button></a>'''
+
+        content = header + fort_paragraph + num_paragraph + another
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
